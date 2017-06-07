ced85b25513e6207c462028ea46d667011602cfe
Brittany Kerr and Steve Spitz <steve@>
2017-06-07 09:10:32 -0700

    create leap year project with JS refactoring

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var leapYear = function(year) {
+      if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+    var result = leapYear(year);
+
+    $("#result").text(result);
+  });
+});
