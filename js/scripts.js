a9e3551e74eb203b6301ff302604339ab7a9da07
Brittany Kerr and Steve Spitz <steve@>
2017-06-07 09:25:18 -0700

    add front-end

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,27 @@
+// business logic
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// user interface logic
 $(document).ready(function() {
-  $("#leap-year").submit(function(event) {
+  $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
-    var leapYear = function(year) {
-      if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-        return true;
-      } else {
-        return false;
-      }
-    };
     var result = leapYear(year);
 
-    $("#result").text(result);
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
